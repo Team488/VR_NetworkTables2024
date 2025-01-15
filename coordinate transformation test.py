12161d7047478b4af02e08ddbfe4e223c264a283
Darrell Teegarden <dat4087@gmail.com>
2025-01-14 20:22:11 -0800

    tests added, gitignore fixed

---
+++ coordinate transformation test.py
@@ -0,0 +1,169 @@
+import unittest
+import numpy as np
+
+def find_transformation_params(points1, points2):
+    points1 = np.array(points1)
+    points2 = np.array(points2)
+
+    centroid1 = np.mean(points1, axis=0)
+    centroid2 = np.mean(points2, axis=0)
+
+    centered_points1 = points1 - centroid1
+    centered_points2 = points2 - centroid2
+
+    H = np.dot(centered_points1.T, centered_points2)
+
+    U, S, Vt = np.linalg.svd(H)
+    R = np.dot(Vt.T, U.T)
+
+    if np.linalg.det(R) < 0:
+        Vt[-1, :] *= -1
+        R = np.dot(Vt.T, U.T)
+
+    var1 = np.var(centered_points1)
+    var2 = np.var(centered_points2)
+    s = np.sqrt(var2 / var1)
+
+    t = centroid2 - s * np.dot(R, centroid1)
+
+    return R, s, t
+
+def transform_coordinates(point, R, s, t):
+    point = np.array(point)
+    transformed_point = s * np.dot(R, point) + t
+    return transformed_point
+
+class TestTransformationParams(unittest.TestCase):
+
+    def setUp(self):
+        # Basic scenario
+        self.points1_basic = [(1, 2), (3, 4), (5, 6)]
+        self.points2_basic = [(2, 4), (6, 8), (10, 12)]
+        self.R_basic, self.s_basic, self.t_basic = find_transformation_params(self.points1_basic, self.points2_basic)
+
+        # Scale scenario
+        self.points1_scale = [(1, 2), (3, 4), (5, 6)]
+        self.points2_scale = [(2, 4), (6, 8), (10, 12)]
+        self.R_scale, self.s_scale, self.t_scale = find_transformation_params(self.points1_scale, self.points2_scale)
+
+        # Rotation scenario
+        angle = np.pi / 4  # 45 degrees
+        self.points1_rotation = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        self.points2_rotation = [(np.cos(angle)*1 - np.sin(angle)*0, np.sin(angle)*1 + np.cos(angle)*0), 
+                                 (np.cos(angle)*0 - np.sin(angle)*1, np.sin(angle)*0 + np.cos(angle)*1), 
+                                 (np.cos(angle)*-1 - np.sin(angle)*0, np.sin(angle)*-1 + np.cos(angle)*0), 
+                                 (np.cos(angle)*0 - np.sin(angle)*-1, np.sin(angle)*0 + np.cos(angle)*-1)]
+        self.R_rotation, self.s_rotation, self.t_rotation = find_transformation_params(self.points1_rotation, self.points2_rotation)
+
+        # Offset scenario
+        self.points1_offset = [(1, 2), (3, 4), (5, 6)]
+        self.points2_offset = [(2, 3), (4, 5), (6, 7)]
+        self.R_offset, self.s_offset, self.t_offset = find_transformation_params(self.points1_offset, self.points2_offset)
+
+        # Custom scenario with provided points
+        self.points1_custom = [(-0.4632839560508728, -3.265570640563965), (-0.2556522488594055, -3.8707668781280518), 
+                               (0.27091512084007263, -4.209332466125488), (0.860622227191925, -4.1687211990356445), 
+                               (1.285730004310608, -3.838834285736084), (1.4859535694122314, -3.257941246032715), 
+                               (1.2625666856765747, -2.6671671867370605), (0.746703028678894, -2.333078384399414), 
+                               (0.13402092456817627, -2.3685243129730225), (-0.32440006732940674, -2.767744541168213)]
+        self.points2_custom = [(np.float64(0.4879177618181647), np.float64(0.8375706683840447)), 
+                               (np.float64(0.903627528513831), np.float64(0.35077864323092456)), 
+                               (np.float64(0.9295111006509376), np.float64(-0.2749499601775298)), 
+                               (np.float64(0.5961643564023811), np.float64(-0.7643143508990771)), 
+                               (np.float64(0.09511872635431252), np.float64(-0.9646454243563674)), 
+                               (np.float64(-0.5013072982981681), np.float64(-0.8296260359390454)), 
+                               (np.float64(-0.904646221977246), np.float64(-0.3481430451656878)), 
+                               (np.float64(-0.9318409805203006), np.float64(0.2669470993622282)), 
+                               (np.float64(-0.5855906425652107), np.float64(0.7724454454361204)), 
+                               (np.float64(-0.012160491581662682), np.float64(0.9692473829108))]
+        self.R_custom, self.s_custom, self.t_custom = find_transformation_params(self.points1_custom, self.points2_custom)
+
+    def test_find_transformation_params_basic(self):
+        expected_R = np.array([[1., 0.], [0., 1.]])
+        expected_s = 2.0
+        expected_t = np.array([0., 0.])
+        
+        np.testing.assert_array_almost_equal(self.R_basic, expected_R)
+        self.assertAlmostEqual(self.s_basic, expected_s)
+        np.testing.assert_array_almost_equal(self.t_basic, expected_t)
+
+    def test_find_transformation_params_scale(self):
+        expected_R = np.array([[1., 0.], [0., 1.]])
+        expected_s = 2.0
+        expected_t = np.array([0., 0.])
+        
+        np.testing.assert_array_almost_equal(self.R_scale, expected_R)
+        self.assertAlmostEqual(self.s_scale, expected_s)
+        np.testing.assert_array_almost_equal(self.t_scale, expected_t)
+
+    def test_find_transformation_params_rotation(self):
+        expected_R = np.array([[np.cos(np.pi / 4), -np.sin(np.pi / 4)], 
+                               [np.sin(np.pi / 4), np.cos(np.pi / 4)]])
+        expected_s = 1.0
+        expected_t = np.array([0., 0.])
+        
+        np.testing.assert_array_almost_equal(self.R_rotation, expected_R)
+        self.assertAlmostEqual(self.s_rotation, expected_s)
+        np.testing.assert_array_almost_equal(self.t_rotation, expected_t)
+
+    def test_find_transformation_params_offset(self):
+        expected_R = np.array([[1., 0.], [0., 1.]])
+        expected_s = 1.0
+        expected_t = np.array([1., 1.])
+        
+        np.testing.assert_array_almost_equal(self.R_offset, expected_R)
+        self.assertAlmostEqual(self.s_offset, expected_s)
+        np.testing.assert_array_almost_equal(self.t_offset, expected_t)
+
+    def test_find_transformation_params_custom(self):
+        # Since we don't have exact expected values for the custom points, 
+        # we'll just ensure the function runs without errors.
+        self.assertIsInstance(self.R_custom, np.ndarray)
+        self.assertIsInstance(self.s_custom, float)
+        self.assertIsInstance(self.t_custom, np.ndarray)
+
+        expected_R = np.array([[ 0.79335161,  0.60876369], [-0.60876369,  0.79335161]])
+        expected_s = 0.9993497563511139
+        expected_t = np.array([1.60327339, 2.90226132])
+
+        np.testing.assert_array_almost_equal(self.R_custom, expected_R)
+        self.assertAlmostEqual(self.s_custom, expected_s)
+        np.testing.assert_array_almost_equal(self.t_custom, expected_t)
+
+    def test_transform_coordinates(self):
+        # Test basic transformation
+        point = (1, 2)
+        expected_transformed_point = np.array([2, 4])
+        transformed_point = transform_coordinates(point, self.R_basic, self.s_basic, self.t_basic)
+        np.testing.assert_array_almost_equal(transformed_point, expected_transformed_point)
+
+        # Test scale transformation
+        point = (1, 2)
+        expected_transformed_point = np.array([2, 4])
+        transformed_point = transform_coordinates(point, self.R_scale, self.s_scale, self.t_scale)
+        np.testing.assert_array_almost_equal(transformed_point, expected_transformed_point)
+
+        # Test rotation transformation
+        point = (1, 0)
+        expected_transformed_point = np.array([np.cos(np.pi / 4), np.sin(np.pi / 4)])
+        transformed_point = transform_coordinates(point, self.R_rotation, self.s_rotation, self.t_rotation)
+        np.testing.assert_array_almost_equal(transformed_point, expected_transformed_point)
+
+        # Test offset transformation
+        point = (1, 2)
+        expected_transformed_point = np.array([2, 3])
+        transformed_point = transform_coordinates(point, self.R_offset, self.s_offset, self.t_offset)
+        np.testing.assert_array_almost_equal(transformed_point, expected_transformed_point)
+
+        # Test custom transformation
+        point = self.points1_custom[0]
+        expected_transformed_point = self.points2_custom[0]
+        transformed_point = transform_coordinates(point, self.R_custom, self.s_custom, self.t_custom)
+        self.assertIsInstance(transformed_point, np.ndarray)
+        np.testing.assert_array_almost_equal(transformed_point, expected_transformed_point)
+        
+
+
+
+if __name__ == '__main__':
+    unittest.main()
