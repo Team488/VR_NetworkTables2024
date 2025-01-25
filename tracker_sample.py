decedf082e9ca4a718515f93351bb26041fb166d
dat4087 <dat4087@gmail.com>
2025-01-25 08:07:49 -0800

    added continuous circle response for offline pose

--- tracker_sample.py
+++ tracker_sample.py
@@ -1,5 +1,5 @@
 import time
-
+import math
 
 start = time.time()
 t_zero = time.time()
@@ -10,14 +10,42 @@ def __sleep(interval):
         time.sleep(sleep_time)
 
 
+def continuous_pose_response(t, radius, x_offset=0, y_offset=0, angle_offset=0, frequency=0.5):
+    period = 1 / frequency  # seconds
+    angle = angle_offset + 2 * math.pi * frequency * t
+    x = x_offset + radius * math.cos(angle)
+    y = y_offset + radius * math.sin(angle)
+    heading = math.atan2(y - y_offset, x - x_offset)
+    return x,y, heading
+
+
 # get and euler pose from the tracker or from an offline function, depending on the mode set by the offlineTest flag
 def get_offline_pose():
-    # return a continuous sine wave response
+    # return a continuous pose response at a frequency of 0.5 Hz, using t_zero as the time reference. 
+    # Calculate both the position and the orientation of the tracker assuming that the tracker is traveling in a circle
+    # at radius r. Include support for an initial x,y offset for the center of the circle, 
+    # and an initial angle offset for the orientation of the tracker.
+    # For each time step, keep the orientation pointing at the center of the circle.
+    global t_zero
+
+    t = time.time() - t_zero
+    frequency = 0.5
+    r = 0.998
+    x_offset = 0.0
+    y_offset = 0.0
+    angle_offset = 0.0
+    x,y, heading = continuous_pose_response(t, r, x_offset, y_offset, angle_offset, frequency)
+
+    # Mapping from the tracker pose to the robot pose
+    # tracker pose order is (x, y, z, roll, pitch, yaw)
+    # x maps to -x
+    # z maps to y
+    # pitch maps to heading
+    pose = (-x, 0, y, 0, heading, 0)
     
-    return (0, 0, 0, 0, 0, 0)
+    return pose
 
 def get_pose(tracker, offlineTest=False):
-    global t_zero
     if offlineTest:
         pose = get_offline_pose()
     else:
